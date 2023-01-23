6eedef0dd4590b4e02f0f8d00360d28d7ae77aa7
agusnt <agusnavarro11@gmail.com>
2023-01-23 12:53:36 +0100

    update nvim

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -6,5 +6,11 @@ require("bufferline").setup{
     options = {
         diagnostics = "nvim_lsp",
         separator_style = {"", ""},
+        modified_icon = '●',
+        numbers = "buffer_id",
+        color_icons = true,
+        show_buffer_icons = true,
+        show_buffer_default_icon = true,
+        show_tab_indicators = true,
     }
 }
