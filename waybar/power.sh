39fc05f6189d45299a35b7418f647041e2db628e
Agustin Navarro-Torres <agustin.navarro@um.es>
2025-02-25 19:55:43 +0100

    waybar update

--- waybar/power.sh
+++ waybar/power.sh
@@ -5,4 +5,5 @@ swaynag -t warning -m 'Power Menu'\
             -b 'Restart' 'shutdown -r now'\
             -b 'Logout' 'swaymsg exit'\
             -b 'Sleep' 'systemctl sleep'\
+            --edge=bottom --border=#d79921\
             --background=#282828 --text=ddc7a1
